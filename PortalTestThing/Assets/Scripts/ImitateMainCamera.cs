c5aa25ec67a28ab3bb05a1322eef830832af07d2
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2018-08-08 01:09:51 +0200

    Camara "imita" jugador (POR TERMINAR)

---
+++ PortalTestThing/Assets/Scripts/ImitateMainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImitateMainCamera : MonoBehaviour {
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = this.GetComponent<Camera>();
+    }
+
+    void Update () {
+        //Invertir giro (?) LA CAMARA HA DE SEGUIR EL PUNTO DE VISTA DEL JUGADOR!
+        cam.transform.rotation = Camera.main.transform.rotation * new Quaternion(-1,0,0,0);
+
+    }
+}
